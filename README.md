32482865f9c17d99a31299215746150a41b2eb20
YinMo19 <Me@YinMo19.top>
2024-12-12 01:58:26 +0800

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # prime_c_lib
+## YinMo19
+
+Use as a library in your C project.
+```sh
+gcc test.c -o test -L<dir> -l<dynamiclib name>
+```
+
+You can get the dylib in ACTION. Download them and unzip, put the 
+prime.h in your project. And 
+```sh
+gcc test.c -o test -L. -lextern_c_test
+```
+
--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 int main() {
     FILE    *f_ptr = fopen("primes.txt", "w");
     uint64_t n     = 0;
-    for (uint64_t i = 0; i < 10000000; i++) {
+    for (uint64_t i = 100000000000000; i < 100000010000000; i++) {
         if (rust_is_prime(i)) {
             fprintf(f_ptr, "%llu\n", i);
         }
